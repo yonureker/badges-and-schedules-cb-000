023aa29674837a19cab358df9a1f80ca41a4e69d
Onur Eker <yonureker@gmail.com>
2017-12-18 19:59:17 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  return array.collect{ |x| badge_maker(x) }
+end
+
+def assign_rooms(array)
+  myArray = []
+  array.each_with_index do |item, index|
+    myArray.push("Hello, #{item}! You'll be assigned to room #{index + 1}")
+  end
+end
