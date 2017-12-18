62f0c151bbeb238d15ee77353db91439361985a1
Onur Eker <yonureker@gmail.com>
2017-12-18 20:21:15 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,5 +7,5 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(array)
-  array.collect.with_index { |item, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!" }
+  array.collect.with_index { |name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!" }
 end
